30a48048d7da6eca58908ddf386a41533fc5cf61
Suman Naresh Jain <138118525+SumanJain2005@users.noreply.github.com>
2024-09-25 23:49:21 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/projects/SNJlogo.jpg";
-import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -11,16 +11,16 @@ const Navbar = () => {
       </div>
       <div className="flex justify-center items-center m-8 text-2xl gap-4">
         <a 
-          href="https://www.facebook.com" 
+          href="https://x.com/Suman_N_Jain" 
           className="mx-2 transition duration-300 text-gray-600 hover:text-white hover:scale-110 hover:shadow-glow" 
           target="_blank" 
           rel="noopener noreferrer" 
-          aria-label="Facebook"
+          aria-label="Twitter"
         >
-          <FaFacebook />
+          <FaTwitter />
         </a>
         <a 
-          href="https://www.github.com" 
+          href="https://github.com/SumanJain2005" 
           className="mx-2 transition duration-300 text-gray-600 hover:text-white hover:scale-110 hover:shadow-glow" 
           target="_blank" 
           rel="noopener noreferrer" 
@@ -29,7 +29,7 @@ const Navbar = () => {
           <FaGithub />
         </a>
         <a 
-          href="https://www.instagram.com" 
+          href="https://www.instagram.com/suman_n_jain/?igsh=eDQ5MG56bTcxd2Zr" 
           className="mx-2 transition duration-300 text-gray-600 hover:text-white hover:scale-110 hover:shadow-glow" 
           target="_blank" 
           rel="noopener noreferrer" 
@@ -38,7 +38,7 @@ const Navbar = () => {
           <FaInstagram />
         </a>
         <a 
-          href="https://www.linkedin.com" 
+          href="https://www.linkedin.com/in/suman-naresh-jain-126a80283/" 
           className="mx-2 transition duration-300 text-gray-600 hover:text-white hover:scale-110 hover:shadow-glow" 
           target="_blank" 
           rel="noopener noreferrer" 
